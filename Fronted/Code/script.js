dee227ec830b86193ab3808ad8d3cd6f2b24f7a1
AniketBhudke <bhudkea@gmail.com>
2025-04-06 18:27:25 +0530

    code

--- Fronted/Code/script.js
+++
@@ -1,8 +0,0 @@
-
-    const menuToggle = document.getElementById('menu-toggle');
-    const dropdownMenu = document.getElementById('dropdown-menu');
-
-    menuToggle.addEventListener('click', () => {
-        dropdownMenu.classList.toggle('active');
-    });
-
